f1c4f9006bcd7a26ebd80ab6bccaa43950ba80d8
Rodrigo Oliveira <rodrigo@janusproj.com>
2019-07-19 18:31:37 -0300

    update img icons

--- packages/janus.extension.chrome.redirect/app/scripts/background.js
+++ packages/janus.extension.chrome.redirect/app/scripts/background.js
@@ -15,18 +15,56 @@ import SmartContract from './core/SmartContract';
 
             if (activeTab && activeTab.url) {
 
-                if (activeTab.url.startsWith('http://jns.') ||
-                    activeTab.url.startsWith('https://www.google.com/search') && activeTab.url.includes('?q=jns.')) {
+                console.log(activeTab.url);
 
-                    console.log('----------' + activeTab.url);
+                let domain;
+                let topDomain;
 
-                    const result = await contract.getStorageHashByDomain('janus', 'eth');
+                if (activeTab.url.startsWith('http://jns.')) {
 
-                    console.log(result);
+                    console.log('starts jns');
 
-                    console.log('---------- true');
-                    chrome.tabs.update(activeTab.id, { url: 'http://ipfs.caralabs.me/ipfs/QmRZCUVAFCTNFkBbYcmNcfsNsZVxQDKZ6rxirkdEwGgBnj/' });
+                    const splitedUrl = activeTab.url.split('.');
+
+                    domain = splitedUrl[1];
+                    topDomain = splitedUrl[2];
+
+                } else if (activeTab.url.startsWith('https://www.google.com/search') && activeTab.url.includes('?q=jns.')) {
+
+                    console.log('redirect google');
+
+                    const params = activeTab.url.split('?');
+
+                    const param = params[1].split('&');
+
+                    const qparam = param[0].split('=');
+
+                    const domains = qparam[1].split('.');
+
+                    domain = domains[1];
+                    topDomain = domains[2];
+
+                    console.log(domain);
+                    console.log(topDomain);
                 }
+
+                let result;
+
+                try {
+                    result = await contract.getStorageHashByDomain(domain, topDomain);
+                } catch (e) {
+                    console.log(e.errorArgs);
+                }
+
+                console.log(result);
+
+                if (result) {
+                    chrome.tabs.update(activeTab.id, { url: 'http://ipfs.caralabs.me/ipfs/' + result + '/' });
+                } else {
+                    console.log(404);
+                }
+
+                window.history.pushState('page', 'title', 'jns.' + domain + '.' + topDomain);
             }
         });
     });
